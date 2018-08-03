e19549822405b7719227f795d369dcd41bb3ac28
phoebe <1321516988@qq.com>
2018-08-03 17:55:28 +0800

    一次修改

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import './Page1.css';
+
+import image from './images/bg.jpg';
 
 export default class Page1 extends Component {
     render() {
         return (
-            <div>
+            <div className="page-box">
                 this is Page1~
+                <img src={image} alt=""/>
             </div>
         )
     }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,12 +3,17 @@ const path = require('path');
 module.exports = {
 
     /*这里是入口*/
-    entry: path.join(__dirname, 'src/index.js'),
+
+    entry: [
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.js')
+    ],
 
     /*这里是输出文件的出口，输出文件到dist文件夹，输出文件名为bundle.js*/
     output: {
         path: path.join(__dirname,'./dist'),
-        filename: 'main.js'
+        filename: 'main.js',
+        chunkFilename: '[name].js'
     },
 
     /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
@@ -18,9 +23,37 @@ module.exports = {
             test: /\.js$/,
             use: ['babel-loader?cacheDirectory=true'],
             include: path.join(__dirname, 'src')
-        }]
+        },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }]
+            }
+        ]
     },
     devServer: {
-        contentBase: path.join(__dirname, './dist')
+        port: 8080,
+        contentBase: path.join(__dirname, './dist'),
+        historyApiFallback: true,
+        host: '0.0.0.0'
+    },
+    resolve: {
+        alias: {
+            pages: path.join(__dirname, 'src/pages'),
+            component: path.join(__dirname, 'src/component'),
+            router: path.join(__dirname, 'src/router'),
+            actions: path.join(__dirname, 'src/redux/actions'),
+            reducers: path.join(__dirname, 'src/redux/reducers'),
+        }
     }
+
+
 };
